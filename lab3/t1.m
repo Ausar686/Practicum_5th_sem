c18c7b3885547a871e3500711599e94bcb7eed3c
GlebYushkov <72838726+GlebYushkov@users.noreply.github.com>
2022-06-04 08:05:21 +0300

    Add files via upload

--- lab3/t1.m
+++ lab3/t1.m
@@ -14,18 +14,18 @@ hold on
 [x1, y1] = ginput(n);
 disp(x1);
 for i=1:size(x1, 1)
-    str = strcat ('-------------- СЂРµС€РµРЅРёРµ в„– ', num2str(i));
+    str = strcat ('-------------- решение № ', num2str(i));
     disp(str)
     answ = fzero (f, x1(i));
     plot(answ, f1(answ), 'b*')
-    str = strcat('РљРѕСЂРµРЅСЊ x = ', num2str(answ));
+    str = strcat('Корень x = ', num2str(answ));
     str = strcat(str, ", y = ");
     str = strcat(str, num2str(f1(answ)));
     disp(str)
-    str = strcat ('РќРµРІСЏР·РєР° = ', abs(num2str(f2(answ) - f1(answ))));
+    str = strcat ('Невязка = ', abs(num2str(f2(answ) - f1(answ))));
     disp(str)
 end
 
-legend('y = cos(x)', 'y = x/\pi', 'РљРѕСЂРЅРё')
+legend('y = cos(x)', 'y = x/\pi', 'Корни')
 hold off;
 clear;
